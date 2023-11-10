9943e6fbb2892e4217848c857f21d043e407e716
Shjuaaj <shivamupadhayay184@gmail.com>
2023-11-10 15:19:24 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded By <span style={{color: "purple", textDecoration:'underline '}}>Shivam Upadhyay</span>.Hosted on Netlify
+    </p>
+  </div>
+  );
+}
